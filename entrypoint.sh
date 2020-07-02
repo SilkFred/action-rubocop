d1e1c03f014fbcbf0608661f287414439cb1dc76
Andy Callaghan <github@andycallaghan.com>
2020-07-02 12:13:01 +0100

    Added base branch attribute, for PR file comparison

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,17 @@ gem install -N rubocop $(version $INPUT_RUBOCOP_VERSION)
 
 echo $INPUT_RUBOCOP_EXTENSIONS | xargs gem install -N
 
+if [ -z ${BASE_BRANCH+x} ]; then
+git diff origin/${BASE_BRANCH} --name-only --diff-filter=AM \
+  | xargs rubocop ${INPUT_RUBOCOP_FLAGS} \
+  | reviewdog -f=rubocop \
+      -name="${INPUT_TOOL_NAME}" \
+      -reporter="${INPUT_REPORTER}" \
+      -filter-mode="${INPUT_FILTER_MODE}" \
+      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+      -level="${INPUT_LEVEL}" \
+      ${INPUT_REVIEWDOG_FLAGS}
+else
 rubocop ${INPUT_RUBOCOP_FLAGS} \
   | reviewdog -f=rubocop \
       -name="${INPUT_TOOL_NAME}" \
@@ -21,3 +32,4 @@ rubocop ${INPUT_RUBOCOP_FLAGS} \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
       -level="${INPUT_LEVEL}" \
       ${INPUT_REVIEWDOG_FLAGS}
+fi
